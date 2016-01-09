15850d688a59f213b9f5a4b774f33fc215a78141
rpdway <alan@csgoarsenal.com>
2016-01-09 18:23:32 -0500

    ave emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session
+
+  # POST /save
+  def save
+    if User.find_by(:email => params[:email])
+      head :error
+    else
+      User.create(:email => params[:email])
+      head :ok
+    end
+  end
+
 end
